f3802b32341d1e4a229a9d7cc1367a58596d5149
Ksenia Voronova <48063501+KVoronova@users.noreply.github.com>
2025-03-12 12:41:35 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Main Slied
 function toggleVideo() {
     const trailer = document.querySelector('.trailer');
     const video = document.querySelector('video');
@@ -25,3 +26,41 @@ function changeTrailer(trl) {
     video.scr = `("/assets/${trl}")`;
 
 }
+
+
+//Filter Movie
+document.addEventListener('DOMContentLoaded', () => {
+    const genreSelect = document.querySelector('select.genre');
+    const yearSelect = document.querySelector('select.year');
+    const movieCards = document.querySelectorAll('.movie-card');
+
+
+    function filterMovies() {
+        const selectedGenre = genreSelect.value;
+        const selectedYear = yearSelect.value;
+
+        movieCards.forEach(card => {
+            const cardGenre = card.querySelector('.genre').textContent.toLowerCase();
+            const cardYear = card.querySelector('.year').textContent;
+
+            const genreMatch = selectedGenre === 'all genre' || cardGenre === selectedGenre;
+            const yearMatch = selectedYear === 'all years' || checkYearRange(cardYear, selectedYear);
+
+            card.style.display = genreMatch && yearMatch ? 'block' : 'none';
+        });
+    }
+
+
+    function checkYearRange(cardYear, selectedYear) {
+        if (selectedYear.includes('-')) {
+            const [startYear, endYear] = selectedYear.split('-').map(Number);
+            return cardYear >= startYear && cardYear <= endYear;
+        } else {
+            return cardYear === selectedYear;
+        }
+    }
+
+
+    genreSelect.addEventListener('change', filterMovies);
+    yearSelect.addEventListener('change', filterMovies);
+});
